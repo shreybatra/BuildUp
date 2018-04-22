cbd5cbaa58292867f15a4bae850ce8be312d8f86
Shrey Batra <shreybatra97@gmail.com>
2018-04-22 15:52:46 +0530

    almost final

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from flask import Flask, jsonify, render_template, request, redirect
 from pymongo import MongoClient
+from geopy import distance
+from geopy.geocoders import Nominatim
 
+
+
+max_dist = 1
 
 client = MongoClient()
 db = client.db
 
 medicines = db.medicines
 users = db.users
-medicines.remove()
+pharmacies = db.phar
+#medicines.remove()
 
 
 app = Flask(__name__)
@@ -21,99 +27,93 @@ def preprocess():
 	#print(type_of(request))
 	
 	if request.method=='POST':
-		'''
-		medicine_name0 = request.form.get('medicine_name0', 'Abc')
-		medicine_qty0 = int(request.form.get('medicine_qty0', 0))
-		medicine_name1 = request.form.get('medicine_name1', None)
-		medicine_qty1 = int(request.form.get('medicine_qty1', 0))
-		medicine_name2 = request.form.get('medicine_name2', None)
-		medicine_qty2 = int(request.form.get('medicine_qty2', 0))
-		medicine_name3 = request.form.get('medicine_name3', None)
-		medicine_qty3 = int(request.form.get('medicine_qty3', 0))
-		medicine_name4 = request.form.get('medicine_name4', None)
-		medicine_qty4 = int(request.form.get('medicine_qty4', 0))
-		#print(request.form['medicine_name0'])
-		
-
-		medicine_name = []
-		medicine_qty = []
-		
-		
-		if medicine_name0 != None and medicine_qty0 != 0:
-			medicine_name.append(medicine_name0)
-			medicine_qty.append(medicine_qty0)
-		if medicine_name1 != None and medicine_qty1 != 0:
-			medicine_name.append(medicine_name1)
-			medicine_qty.append(medicine_qty1)
-		if medicine_name2 != None and medicine_qty2 != 0:
-			medicine_name.append(medicine_name2)
-			medicine_qty.append(medicine_qty2)
-		if medicine_name3 != None and medicine_qty3 != 0:
-			medicine_name.append(medicine_name3)
-			medicine_qty.append(medicine_qty3)
-		if medicine_name4 != None and medicine_qty4 != 0:
-			medicine_name.append(medicine_name4)
-			medicine_qty.append(medicine_qty4)
-		
-
-
-		lat = float(request.form['lat'])
-		lng = float(request.form['lng'])
-		'''
-
-		medicine_name0 = request.json.get('medicine_name0', 'Abc')
-		medicine_qty0 = int(request.json.get('medicine_qty0', 0))
-		medicine_name1 = request.json.get('medicine_name1', None)
-		medicine_qty1 = int(request.json.get('medicine_qty1', 0))
-		medicine_name2 = request.json.get('medicine_name2', None)
-		medicine_qty2 = int(request.json.get('medicine_qty2', 0))
-		medicine_name3 = request.json.get('medicine_name3', None)
-		medicine_qty3 = int(request.json.get('medicine_qty3', 0))
-		medicine_name4 = request.json.get('medicine_name4', None)
-		medicine_qty4 = int(request.json.get('medicine_qty4', 0))
 		#print(request.json['medicine_name0'])
 		
 
-		medicine_name = []
-		medicine_qty = []
-		
-		
-		if medicine_name0 != None and medicine_qty0 != 0:
-			medicine_name.append(medicine_name0)
-			medicine_qty.append(medicine_qty0)
-		if medicine_name1 != None and medicine_qty1 != 0:
-			medicine_name.append(medicine_name1)
-			medicine_qty.append(medicine_qty1)
-		if medicine_name2 != None and medicine_qty2 != 0:
-			medicine_name.append(medicine_name2)
-			medicine_qty.append(medicine_qty2)
-		if medicine_name3 != None and medicine_qty3 != 0:
-			medicine_name.append(medicine_name3)
-			medicine_qty.append(medicine_qty3)
-		if medicine_name4 != None and medicine_qty4 != 0:
-			medicine_name.append(medicine_name4)
-			medicine_qty.append(medicine_qty4)
-		
+		medicine_name = request.json['medicines']
+		medicine_qty = request.json['quantities']
+		lat = float(request.json.get('lat',0))
+		lng = float(request.json.get('lng',0))
 
-
+		req = {}
+		req['medicine_name'] = medicine_name
+		req['medicine_qty'] = medicine_qty
+		req['lat'] = lat
+		req['lng'] = lng
+		
+		
+		
 		#lat = float(request.json['lat'])
 		#lng = float(request.json['lng'])
 
-
+		'''
 		entry = {
 			'medicine_name':medicine_name,
 			'medicine_qty':medicine_qty
-			#'lat':lat,
-			#'lng':lng
 		}
+
 
 		#print(entry)
 		medicines.insert_one(entry)
+		'''
 
+		result = pharmacy(req)
 		
-		return jsonify({'inserted':'entry'}),201
+		return jsonify({'Result':result}),201
 	
 	
+def cmp_items(a, b):
+    if a.count > b.count:
+        return 1
+    elif a.count < b.count:
+        return 0
+    else:
+        return -1
+
+
+def pharmacy(req):
+
+	pharma = pharmacies.find()
+
+	print(req)
+	#print(list(pharma))
+
+	if pharma.count()==0:
+		return jsonify({'none':'found'})
+
+	ans = []
+
+	for p in pharma:
+		count = 0
+		#geolocator = Nominatim()
+		#location = geolocator.reverse((p['lat'],p['lng']),timeout=10)
+		#print(location.address)
+		if distance.distance( (req['lat'],req['lng']), (p['lat'],p['lng']) ).km <= max_dist:
+			for i,med in enumerate(req['medicine_name']):
+				#print(med)
+				r = [c for c in p['medicines'] if c['med_name'].lower()==med.lower()]
+				if len(r)==0:
+					continue
+				else:
+					r = r[0]
+					if int(req['medicine_qty'][i])<=r['med_qty']:
+						count += 1
+		
+		if count!=0:
+			pp = {}
+			pp['count'] = count
+			pp['name'] = p['pharma_name']
+			ans.append(pp)
+
+	ans.sort(key=lambda a: a['count'])
+
+	#print(ans)
+	return ans
+
+
+
+
+
 
 @app.route('/hello', methods=['GET'])
 def hello():
